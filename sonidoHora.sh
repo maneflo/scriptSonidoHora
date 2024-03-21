aa955599067eba469ee8afe9c10bbe64a4c93515
Juan Flores <juanagustinf@gmail.com>
2024-03-20 21:27:00 -0300

    cambie por completo el script
    tenia errores de desbordamiento, mal manejo de variables, este si funciona.

--- sonidoHora.sh
+++ sonidoHora.sh
@@ -1,22 +1,27 @@
-#!/user/bin/bash
- 
+#!/bin/bash
+
 # Archivo de sonido
 sonido="/usr/share/sounds/sonido.aiff"
 sonido1="/usr/share/sounds/sonido1.wav"
 
 while true; do
   # Obtener la hora actual
-  hora_actual=$(date +"%M")
+  hora_actual=$(date +"%H:%M")
+#pruebas para ver porque no sonaba cuando quería. lo que me tiraba gemini no dió los resultados.
 #echo $hora_actual
-  # Si el minuto es 00, reproducir el sonido
-  if [[ $hora_actual -eq 00 ]]; then
-    paplay "$sonido" & 
+#  minutos=$(echo "$hora_actual" | cut -c 4-5)
+#echo $minutos
+  # Si el minuto es 00  reproducir el sonido
+  if [[ "$minutos" -eq 00  ]]; then
+    paplay "$sonido" &
   fi
-# sonido para la media hora
-	if [[ $hora_actual -eq 30 ]]; then 
-	paplay "$sonido1" &
-	fi
-	
+#echo "hora "
+  # Si el minuto es 30, reproducir el sonido1
+  if [[ "$minutos" -eq 30 ]]; then
+    paplay "$sonido1" &
+  fi
+#echo "mediaHOra"
   # Esperar 60 segundos
   sleep 60
 done
+
